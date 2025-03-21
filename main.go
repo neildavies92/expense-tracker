4fb44c624490c2c650bd0dc0db1b6870bbfe551a
Neil Davies <neil.davies@thrivelearning.com>
2025-03-21 10:26:27 +0000

    create expense file with read and write permissions

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	file, err := os.OpenFile("expenses.txt", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("File cannot be created: %v", err)
+	}
+	defer file.Close()
+
+	if _, err := os.Stat("expenses.txt"); os.IsNotExist(err) {
+		fmt.Println("File succesfully created.")
+	} else {
+		fmt.Println("Skipped creating... file already exists.")
+	}
 }
