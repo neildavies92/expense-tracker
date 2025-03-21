cf21a554470c2a86f9bdef85c8228cb6557a187b
Neil Davies <neil.davies@thrivelearning.com>
2025-03-21 17:09:37 +0000

    update main function and test data

--- main.go
+++ main.go
@@ -15,21 +15,22 @@ func CreateExpenseFile(filename string) {
 	fmt.Println("File created successfully...")
 }
 
-func WriteToFile(filename string, data string) {
+func WriteToFile(filename string, expense string) {
 	expenseFile, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatalf("File cannot be opened: %v", err)
 	}
 	defer expenseFile.Close()
 
-	_, err = expenseFile.WriteString(data)
+	_, err = expenseFile.WriteString(expense)
 	if err != nil {
 		log.Fatalf("Data cannot be written to file: %v", err)
 	}
-	fmt.Println("Data written successfully...")
+	fmt.Println("New expense added to file...")
 }
 
 func main() {
-	CreateExpenseFile("expenses.txt")
-	WriteToFile("expenses.txt", "Hello World\n")
+	file := "expenses.txt"
+	CreateExpenseFile(file)
+	WriteToFile(file, "Car: £331.11\n")
 }
