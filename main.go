1be38b3e2d3a6b6b3fe9bcd112b960082cebc2af
Neil Davies <neil.davies@thrivelearning.com>
2025-03-21 17:20:08 +0000

    take user input and write to file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -15,12 +16,20 @@ func CreateExpenseFile(filename string) {
 	fmt.Println("File created successfully...")
 }
 
-func WriteToFile(filename string, expense string) {
+func WriteToFile(filename string) {
 	expenseFile, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatalf("File cannot be opened: %v", err)
 	}
 	defer expenseFile.Close()
+
+	welcome := "Welcome to the expense tracker!\n"
+	fmt.Println(welcome)
+
+	fmt.Println("Add a new expense name: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	expense, _ := reader.ReadString('\n')
 
 	_, err = expenseFile.WriteString(expense)
 	if err != nil {
@@ -32,5 +41,5 @@ func WriteToFile(filename string, expense string) {
 func main() {
 	file := "expenses.txt"
 	CreateExpenseFile(file)
-	WriteToFile(file, "Car: £331.11\n")
+	WriteToFile(file)
 }
